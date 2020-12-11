9c64fdb6f4ae45c199fb7124a570d9c944ddbb41
Gleisson Neves <53718407+gdk46@users.noreply.github.com>
2020-12-11 01:39:06 -0300

    documentação

---
+++ doc/dump/caseimobiliaria- com dados.sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2020 at 05:05 AM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `caseimobiliaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contrato`
+--
+
+CREATE TABLE `contrato` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_locatario` int(10) UNSIGNED NOT NULL,
+  `id_locador` int(10) UNSIGNED NOT NULL,
+  `id_imovel` int(10) UNSIGNED NOT NULL,
+  `data_inicio` date DEFAULT NULL,
+  `data_fim` date DEFAULT NULL,
+  `taxa_administrativa` double DEFAULT NULL,
+  `valor_aluguel` double DEFAULT NULL,
+  `valor_condominio` double DEFAULT NULL,
+  `valor_iptu` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `contrato`
+--
+
+INSERT INTO `contrato` (`id`, `id_locatario`, `id_locador`, `id_imovel`, `data_inicio`, `data_fim`, `taxa_administrativa`, `valor_aluguel`, `valor_condominio`, `valor_iptu`) VALUES
+(1, 1, 1, 1, '2020-12-11', NULL, 100, 100, 100, 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imovel`
+--
+
+CREATE TABLE `imovel` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_proprietario` int(10) UNSIGNED NOT NULL,
+  `cep` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `logradouro` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `complemento` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `bairro` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `cidade` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `uf` varchar(255) CHARACTER SET utf8 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `imovel`
+--
+
+INSERT INTO `imovel` (`id`, `id_proprietario`, `cep`, `logradouro`, `complemento`, `bairro`, `cidade`, `uf`) VALUES
+(1, 1, '49001-356', 'Rua B', 'test', 'Zona de Expansão (Aruana)', 'test', 'SC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locador`
+--
+
+CREATE TABLE `locador` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nome` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefone` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `locador`
+--
+
+INSERT INTO `locador` (`id`, `nome`, `email`, `telefone`) VALUES
+(1, 'dj123', '123@gmail.com', '(82) 9 9833-3377');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locatario`
+--
+
+CREATE TABLE `locatario` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `telefone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `locatario`
+--
+
+INSERT INTO `locatario` (`id`, `nome`, `email`, `telefone`) VALUES
+(1, 'teste santos', 'teste@gmailcom', '(82) 9 9833-3377');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mensalidade`
+--
+
+CREATE TABLE `mensalidade` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `indice` int(11) DEFAULT NULL,
+  `id_contrato` int(10) UNSIGNED NOT NULL,
+  `valor_mensalidade` double DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  `data` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `mensalidade`
+--
+
+INSERT INTO `mensalidade` (`id`, `indice`, `id_contrato`, `valor_mensalidade`, `status`, `data`) VALUES
+(1, 0, 1, 300, 'EM ABERTO', '2021-01-11'),
+(2, 1, 1, 300, 'EM ABERTO', '2021-02-11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `repasse`
+--
+
+CREATE TABLE `repasse` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `indice` int(11) DEFAULT NULL,
+  `id_contrato` int(10) UNSIGNED NOT NULL,
+  `valor_repasse` double NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `data` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `repasse`
+--
+
+INSERT INTO `repasse` (`id`, `indice`, `id_contrato`, `valor_repasse`, `status`, `data`) VALUES
+(1, 0, 1, 100, 'EM ABERTO', '2021-01-16'),
+(2, 1, 1, 100, 'EM ABERTO', '2021-02-16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contrato`
+--
+ALTER TABLE `contrato`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contrato_ibfk_3` (`id_imovel`),
+  ADD KEY `id_locador` (`id_locador`),
+  ADD KEY `id_locatario` (`id_locatario`);
+
+--
+-- Indexes for table `imovel`
+--
+ALTER TABLE `imovel`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_proprietario` (`id_proprietario`);
+
+--
+-- Indexes for table `locador`
+--
+ALTER TABLE `locador`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `locatario`
+--
+ALTER TABLE `locatario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mensalidade`
+--
+ALTER TABLE `mensalidade`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `menasalidade_ibfk_1` (`id_contrato`);
+
+--
+-- Indexes for table `repasse`
+--
+ALTER TABLE `repasse`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_contrato` (`id_contrato`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contrato`
+--
+ALTER TABLE `contrato`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `imovel`
+--
+ALTER TABLE `imovel`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `locador`
+--
+ALTER TABLE `locador`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `locatario`
+--
+ALTER TABLE `locatario`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `mensalidade`
+--
+ALTER TABLE `mensalidade`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `repasse`
+--
+ALTER TABLE `repasse`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contrato`
+--
+ALTER TABLE `contrato`
+  ADD CONSTRAINT `contrato_ibfk_3` FOREIGN KEY (`id_imovel`) REFERENCES `imovel` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `contrato_ibfk_4` FOREIGN KEY (`id_locador`) REFERENCES `locador` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `contrato_ibfk_5` FOREIGN KEY (`id_locatario`) REFERENCES `locatario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `imovel`
+--
+ALTER TABLE `imovel`
+  ADD CONSTRAINT `imovel_ibfk_1` FOREIGN KEY (`id_proprietario`) REFERENCES `locador` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `mensalidade`
+--
+ALTER TABLE `mensalidade`
+  ADD CONSTRAINT `mensalidade_ibfk_1` FOREIGN KEY (`id_contrato`) REFERENCES `contrato` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `repasse`
+--
+ALTER TABLE `repasse`
+  ADD CONSTRAINT `repasse_ibfk_1` FOREIGN KEY (`id_contrato`) REFERENCES `contrato` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
